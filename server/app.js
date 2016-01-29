5b1b1109ec64d49f0caafa2058acad66895e5f8a
João Tarla <joao@99taxis.com>
2016-01-29 16:24:51 -0200

    Fixes first test

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/healthCheck', function(err, res){
-  res.send('ok');
+  res.send('I\'m alive!');
 });
 
 app.listen(8080);
--- test/test-server.js
+++ test/test-server.js
@@ -5,7 +5,12 @@ var should   = chai.should();
 chai.use(require('chai-http'));
 
 describe('Router', function() {
-  it('should pass', function(done) {
-      done();
-    });
+  it('should correctly works on healhtCheck GET', function(done) {
+      chai.request(app)
+        .get('/healthCheck')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+  });
 });
